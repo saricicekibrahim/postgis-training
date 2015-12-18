364b71fff044aee41e7dfa5e82d8d17ad4e8f91c
saricicek-pc <ibrahimsaricicek@gmail.com>
2015-12-18 22:56:32 +0200

    other workshops

---
+++ workshop2.sql
@@ -0,0 +1,49 @@
+﻿CREATE TABLE spatialdata2
+(
+id serial NOT NULL,
+geom geometry,
+CONSTRAINT spatialdata2_pkey PRIMARY KEY (id)
+)
+
+alter table spatialdata2 add column geom2 geometry;
+
+insert into spatialdata2 (geom)
+select st_setsrid(st_makeline(st_makepoint(26.809505,40.055404),
+st_makepoint(29.462703,38.666675)),4326)
+
+select st_srid(geom) from spatialdata2
+update spatialdata2 set geom =st_setsrid(geom,432)
+
+update spatialdata2 set geom2 = st_transform(geom,900913)
+
+select st_isvalid(geom) from tr_iller where 
+st_isvalid(geom) = 'f'
+
+select st_astext(st_startpoint(geom)),
+st_x(st_startpoint(geom)),
+st_y(st_startpoint(geom))
+ from spatialdata where
+st_geometrytype(geom) = 'ST_LineString'
+
+
+select iladi, st_npoints(geom) from tr_iller limit 5
+
+select st_astext(st_pointn(geom,1)),
+ st_astext(st_geometryn(geom,1)) from spatialdata
+
+ select st_dwithin(
+	st_endpoint(geom),
+		st_geomfromtext('POINT(29.461703 38.666675)',4326)
+	,0.005)
+ from spatialdata
+ where
+st_geometrytype(geom) = 'ST_LineString'
+
+
+select st_intersects(
+	st_buffer(st_endpoint(geom), 0.005),
+		st_geomfromtext('POINT(29.461703 38.666675)',4326)
+	)
+ from spatialdata
+ where
+st_geometrytype(geom) = 'ST_LineString'
---
+++ workshop3.sql
@@ -0,0 +1,58 @@
+﻿select iladi from spatialdata s, tr_iller i 
+where 
+st_geometrytype(s.geom) = 'ST_Point'
+and
+st_x(s.geom) > st_xmin(i.geom);
+
+
+select iladi, st_distance(s.geom,i.geom), s.geom <#> i.geom from spatialdata s, tr_iller i 
+where 
+st_geometrytype(s.geom) = 'ST_Point'
+and
+--s.geom <-> i.geom =0
+s.geom && i.geom;
+
+
+
+insert into spatialdata(geom)
+select 
+st_transform(
+	st_setsrid(
+		st_makepoint(
+			st_x(
+				st_transform(geom,900913))-10000,
+			st_y(
+				st_transform(geom,900913)) + 20000
+				) 
+				,900913)
+				,4326)
+from spatialdata where
+st_geometrytype(geom) = 'ST_Point'
+
+
+select st_centroid(geom) from tr_iller limit 5
+
+
+select st_distance(r.geom, a.geom)
+from tr_iller r, tr_iller a
+where
+r.iladi='Rize' and
+a.iladi='Ankara'
+
+
+select st_distance(
+st_transform(st_centroid(r.geom),900913), 
+st_transform(st_centroid(a.geom),900913)
+)/1000 || ' km'
+from tr_iller r, tr_iller a
+where
+r.iladi='Rize' and
+a.iladi='Ankara'
+
+select 
+i.iladi,
+st_distance(a.geom,i.geom) as d from tr_iller i, tr_iller a
+where 
+a.iladi='Ankara'
+order by d desc
+limit 1
