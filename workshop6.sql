052d60e97df3f6538308f61e59b78b50438098a6
saricicek-pc <ibrahimsaricicek@gmail.com>
2015-12-28 14:41:20 +0200

    workshop 6 -7

---
+++ workshop6.sql
@@ -0,0 +1,13 @@
+﻿
+--noktaya 20 metre yakındaki yolun adını ve yolun
+--bu noktaya en yakın noktasını getirir
+select 
+name, 
+st_closestpoint(
+	geom,
+	st_geomfromtext('POINT(32.78981 39.95935)',4326)) 
+	as geom
+ from roads
+where st_dwithin(geom,
+st_geomfromtext('POINT(32.78981 39.95935)',4326),0.0002) 
+limit 1
---
+++ workshop7.sql
@@ -0,0 +1,33 @@
+﻿CREATE TYPE route_intersect_type_v1 AS
+   (distance double precision,
+   istouching boolean
+   );
+--idleri tanımlı iki yolun bboxları arasındaki uzaklığı döndüren fonksiyon
+--uzaklık 0 ise istouching değerini true set eder
+CREATE OR REPLACE FUNCTION route_intersect_v1(
+    int1 integer,
+    int2 integer)
+  RETURNS route_intersect_type_v1 AS
+$BODY$
+	DECLARE
+		route1 text;
+		route2 text;
+		returnVal route_intersect_type_v1;
+	BEGIN
+		select geom from roads where ogc_fid = int1 into route1;
+		select geom from roads where ogc_fid = int2 into route2;
+
+		select route1 <#> route2 into returnVal.distance;
+
+		returnVal.istouching = false;
+		if returnVal.distance = 0 then
+		returnVal.istouching = true;
+		end if;		
+		
+		return returnVal;
+	END;
+	$BODY$
+  LANGUAGE plpgsql VOLATILE STRICT
+  COST 100;
+
+  select route_intersect_v1(1806,13621);
