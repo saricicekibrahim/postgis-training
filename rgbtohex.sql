4738bcab7cd74572219be7bf3d732d28dd6d1678
İbrahim Sarıçiçek <ibrahimsaricicek@gmail.com>
2018-05-10 17:03:42 +0300

    Postgresql rgb to hex color converter function

---
+++ rgbtohex.sql
@@ -0,0 +1,28 @@
+CREATE OR REPLACE FUNCTION rgbtohex(_rgb text) RETURNS text AS $$
+	declare rgbarr text[];
+	red text;
+	green text;
+	blue text;
+	
+        BEGIN
+		select regexp_split_to_array(_rgb, ',') into rgbarr;
+		red = to_hex(rgbarr[1]::integer);
+		green = to_hex(rgbarr[2]::integer);
+		blue = to_hex(rgbarr[3]::integer);
+
+		if length(red) = 1 then
+		red = '0' || red;
+		END IF;
+
+		if length(green) = 1 then
+		green = '0' || green;
+		END IF;
+
+		if length(blue) = 1 then
+		blue = '0' || blue;
+		END IF;
+                RETURN '#' || red || green || blue;
+        END;
+$$ LANGUAGE plpgsql;
+
+SELECT mobiliz_rgbtohex('0, 51, 255');
